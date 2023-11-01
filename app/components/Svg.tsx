ee945cd89d502794a5942360c627eb321b0e7f15
Dušan Miletić <d@miletich.cc>
2023-11-01 19:37:32 +0100

    add Svg

---
+++ app/components/Svg.tsx
@@ -0,0 +1,22 @@
+import { type ComponentProps, forwardRef } from 'react';
+
+type Props = ComponentProps<'svg'> & {
+  width: number;
+  height: number;
+};
+
+export default forwardRef<SVGSVGElement, Props>(function Svg(
+  { className = '', children, width, height, ...rest },
+  ref
+) {
+  return (
+    <svg
+      ref={ref}
+      className={` ${className}`}
+      {...rest}
+      viewBox={`0 0 ${width} ${height}`}
+    >
+      {children}
+    </svg>
+  );
+});
