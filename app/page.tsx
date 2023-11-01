76db5f45b1dfe3f585b23e152bcf5b7128ef145c
Dušan Miletić <d@miletich.cc>
2023-11-01 18:23:04 +0100

    add initial markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image';
 import { getData } from './data';
+import H2 from './components/H2';
+import P from './components/P';
 
 export default async function Home() {
   const data = await getData();
 
-  return null;
+  return (
+    <header className="flex flex-col items-center">
+      <H2>Daily Temperature Ranges</H2>
+      <P>Daily minimum and maximum temperatures in Smederevo in 2022</P>
+    </header>
+  );
 }
