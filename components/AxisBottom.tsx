2574fa4dc5390d0e7d93d1bdd1b698e85f12977b
Dušan Miletić <d@miletich.cc>
2023-11-02 17:08:32 +0100

    add AxisBottom

---
+++ components/AxisBottom.tsx
@@ -0,0 +1,28 @@
+import { gray, tickLabelOffset, tickLabelSize, tickSize } from '@/utils/consts';
+import { ScaleTick } from '@/utils/scales';
+
+type Props = {
+  ticks: ScaleTick[];
+  length: number;
+};
+
+export default function AxisBottom({ ticks, length }: Props) {
+  return (
+    <g className="axis-bottom">
+      <line x1={0} x2={length} y1={length} y2={length} stroke={gray} />
+      {ticks.map(({ label, position }) => (
+        <g key={label} transform={`translate(${position}, ${length})`}>
+          <line y1={0} y2={tickSize} stroke={gray} />
+          <text
+            dy={tickLabelOffset}
+            fontSize={tickLabelSize}
+            textAnchor="middle"
+            alignmentBaseline="hanging"
+          >
+            {label}
+          </text>
+        </g>
+      ))}
+    </g>
+  );
+}
