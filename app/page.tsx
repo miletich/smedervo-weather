3afb7c32d23c3d2eeefbdaa503b3d4e425525fda
Dušan Miletić <d@miletich.cc>
2023-11-02 17:13:10 +0100

    draw axes

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,16 @@ import H2 from '../components/H2';
 import P from '../components/P';
 import Chart from '@/components/Chart';
 import { dimensions, dotSize } from '@/utils/consts';
+import AxisLeft from '@/components/AxisLeft';
+import { getScaleTicks, getScales } from '@/utils/scales';
 import { getDotProps } from '@/utils/lib';
+import AxisBottom from '@/components/AxisBottom';
 
 export default async function Home() {
   const dots = await getDotProps();
+  const { xScale, yScale } = await getScales();
+  const xScaleTicks = getScaleTicks({ scale: xScale, tickCount: 4 });
+  const yScaleTicks = getScaleTicks({ scale: yScale, tickCount: 4 });
 
   return (
     <main>
@@ -28,6 +34,8 @@ export default async function Home() {
           y={0}
           fill="white"
         />
+        <AxisBottom ticks={xScaleTicks} length={dimensions.innerWidth} />
+        <AxisLeft ticks={yScaleTicks} length={dimensions.innerHeight} />
         <g className="dots">
           {dots.map((d, i) => (
             <circle key={`${d.cx}-${d.cy}-${i}`} r={dotSize} {...d} />
