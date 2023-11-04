429a5fb0dce1317e6add2dead96ac1d1a2322b39
Dušan Miletić <d@miletich.cc>
2023-11-04 21:55:11 +0100

    add Tooltip

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import ScatterPlot from '@/sections/ScaterPlot';
 import { CurrentDateContextProvider } from '@/interaction/CurrentDateContext';
 import MouseLeaveController from '@/interaction/MouseLeaveController';
 import DotHighlight from '@/interaction/DotHighlight';
+import Tooltip from '@/interaction/Tooltip';
 
 export default async function Home() {
   return (
@@ -18,7 +19,7 @@ export default async function Home() {
           dimensions={dimensions}
           role="img"
           aria-labelledby="sdWeatherTitle sdWeatherDescription"
-          className="w-[75vw] h-[75vh]"
+          className="w-full h-full sm:w-[75vh] sm:h-[75vh]"
         >
           <MouseLeaveController>
             <Meta />
@@ -29,6 +30,7 @@ export default async function Home() {
             <DotHighlight />
             <GradientLegend />
           </MouseLeaveController>
+          <Tooltip />
         </Chart>
       </CurrentDateContextProvider>
     </main>
---
+++ interaction/Tooltip.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { useCurrentDateData } from './CurrentDateContext';
+import { dateAccessor, tempMaxAccessor, tempMinAccessor } from '@/utils/data';
+import { useLayoutEffect, useRef, useState } from 'react';
+import {
+  tooltipOffset,
+  tooltipPadding,
+  tooltipFontSize,
+  tooltipLineHeight,
+  tooltipFontSizeLg,
+  darkGray,
+} from '@/utils/consts';
+import TooltipSvg from '@/components/TooltipSvg';
+
+type Position = Record<'top' | 'left', number>;
+
+export default function Tooltip() {
+  const tooltipHeight =
+    tooltipFontSizeLg +
+    tooltipFontSize * tooltipLineHeight +
+    tooltipPadding * 2;
+  const currentDate = useCurrentDateData();
+  const firstLineRef = useRef<SVGTextElement>(null);
+  const secondLineRef = useRef<SVGTextElement>(null);
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    const firstLineWidth = firstLineRef.current?.getBBox().width || 0;
+    const secondLineWidth = secondLineRef.current?.getBBox().width || 0;
+
+    setWidth(Math.max(firstLineWidth, secondLineWidth));
+  }, [currentDate]);
+
+  if (!currentDate) return null;
+
+  const { datum, center } = currentDate;
+
+  return (
+    <g
+      className="tooltip pointer-events-none"
+      transform={`translate(${center[0]}, ${center[1]})`}
+    >
+      <TooltipSvg
+        offset={tooltipOffset}
+        width={width + tooltipPadding * 2}
+        height={tooltipHeight}
+      />
+      <text
+        ref={firstLineRef}
+        fontSize={tooltipFontSize}
+        y={-tooltipHeight + tooltipPadding}
+        textAnchor="middle"
+        fontWeight={600}
+        fill={darkGray}
+      >
+        {dateAccessor(datum).toLocaleDateString('en-GB', {
+          year: 'numeric',
+          month: 'long',
+        })}
+      </text>
+      <text
+        ref={secondLineRef}
+        fontSize={tooltipFontSize}
+        textAnchor="middle"
+        fill={darkGray}
+        fillOpacity={0.8}
+        y={
+          -tooltipHeight + tooltipPadding + tooltipFontSize * tooltipLineHeight
+        }
+      >
+        {`${tempMinAccessor(datum).toFixed()}°C - ${tempMaxAccessor(
+          datum
+        ).toFixed()}°C`}
+      </text>
+    </g>
+  );
+}
