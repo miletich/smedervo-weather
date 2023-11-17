3c24c32c3146f5fcdfaa905a09d4bbbd18503b40
Dušan Miletić <d@miletich.cc>
2023-11-17 18:13:05 +0100

    add top histogram highlight

--- sections/TopHistogram.tsx
+++ sections/TopHistogram.tsx
@@ -4,17 +4,25 @@ import {
   histogramOpacity,
   midGray,
 } from '@/utils/consts';
-import { getTopHistogramArea } from '@/utils/lib';
+import { getData, getTopHistogramArea } from '@/utils/lib';
+import TopHistogramHighlight from './TopHistogramHighlight';
 
 export default async function TopHistogram() {
+  // main histogram
   const d = await getTopHistogramArea();
 
+  // highlight
+  const data = await getData();
+
   return (
-    <path
-      d={d}
-      fill={midGray}
-      opacity={histogramOpacity}
-      transform={`translate(0 ${-histogramHeight - histogramMargin})`}
-    />
+    <>
+      <path
+        d={d}
+        fill={midGray}
+        opacity={histogramOpacity}
+        transform={`translate(0 ${-histogramHeight - histogramMargin})`}
+      />
+      <TopHistogramHighlight data={data} />
+    </>
   );
 }
