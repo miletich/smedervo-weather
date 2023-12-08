cb89eb214b6877c46db7112315de2d8c52900053
Dušan Miletić <d@miletich.cc>
2023-12-08 15:03:57 +0100

    init home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
 import type { PropsWithChildren } from 'react';
 
 import H2 from '@/components/H2';
+import P from '@/components/P';
+import MainMenuItem, { MainMenu } from '@/components/MainMenu';
 
-export default async function Home({ children }: PropsWithChildren) {
+import ScatterPlotIllustration from './scatter-plot/Illustration';
+
+export default async function Home() {
   return (
     <>
       <header className="flex flex-col items-center">
         <H2>Smederevo 2022 weather overview</H2>
       </header>
-      <main>{children}</main>
+      <main className="w-full sm:w-3/4 sm:h-3/4">
+        <MainMenu>
+          <MainMenuItem href="/scatter-plot">
+            <ScatterPlotIllustration />
+            <P>Scatter Plot</P>
+          </MainMenuItem>
+          <MainMenuItem href="/radar">
+            <P className="w-[90%] aspect-square flex items-center justify-center m-[5%] bg-zinc-100">
+              WIP
+            </P>
+            <P>Radar</P>
+          </MainMenuItem>
+        </MainMenu>
+      </main>
     </>
   );
 }
