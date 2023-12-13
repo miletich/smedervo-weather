7d1f9011d40af3895cd504de647e17cf661aad41
Dušan Miletić <d@miletich.cc>
2023-12-13 16:24:54 +0100

    define columns

--- app/table/utils/tableDef.tsx
+++ app/table/utils/tableDef.tsx
@@ -1,4 +1,52 @@
-import { Datum } from '@/utils/data';
 import { createColumnHelper } from '@tanstack/table-core';
 
+import type { Datum } from '@/utils/data';
+
 export const columnHelper = createColumnHelper<Datum>();
+
+const columns = [
+  columnHelper.accessor('datetime', {
+    header: () => 'Date',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('tempmin', {
+    header: () => 'Min Temp',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('description', {
+    header: () => 'Description',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('tempmax', {
+    header: () => 'Max Temp',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('sunrise', {
+    header: () => 'Sunrise',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('sunset', {
+    header: () => 'Sunset',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('uvindex', {
+    header: () => 'UV Index',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('preciptype', {
+    header: () => 'Precipitation',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('windspeed', {
+    header: () => 'Wind Speed',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('winddir', {
+    header: () => 'Wind Direction',
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('moonphase', {
+    header: () => 'Moon Phase',
+    cell: (info) => info.getValue(),
+  }),
+];
