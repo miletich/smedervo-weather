6696d74c03101f26287d6b17c2f21a2b67fd5aec
Dušan Miletić <d@miletich.cc>
2023-12-15 17:01:09 +0100

    add Arrow

---
+++ app/table/components/Wind/Arrow.tsx
@@ -0,0 +1,40 @@
+import { type ComponentProps, forwardRef } from 'react';
+
+import Svg from '@/components/Svg';
+
+import { black, darkGray } from '../../consts';
+
+type Props = ComponentProps<'svg'> & {
+  direction: number;
+};
+
+const size = 4;
+
+export default forwardRef<SVGSVGElement, Props>(function Arrow(
+  { className = '', direction, ...rest },
+  ref
+) {
+  const d = `
+    M 2,0
+    L 3,4
+    L 2,3
+    L 1,4
+    L 2,0z
+  `;
+  return (
+    <Svg
+      className={` ${className}`}
+      {...rest}
+      width={size}
+      height={size}
+      ref={ref}
+    >
+      <path
+        d={d}
+        transform={`rotate(${direction} ${size / 2} ${size / 2})`}
+        rotate={direction}
+        fill={black}
+      />
+    </Svg>
+  );
+});
