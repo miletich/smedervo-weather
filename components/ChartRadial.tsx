275182d2b5f63a30d35dc85aed9b6b00b103c73d
Dušan Miletić <d@miletich.cc>
2023-12-21 18:42:21 +0100

    migrate ChartRadial to stylex

--- components/ChartRadial.tsx
+++ components/ChartRadial.tsx
@@ -1,17 +1,25 @@
 import { type ComponentProps, forwardRef } from 'react';
-import Svg from './Svg';
+import * as stylex from '@stylexjs/stylex';
+
 import { RadialDimensions } from '@/utils/dimensions';
 
-type Props = ComponentProps<'svg'> & { dimensions: RadialDimensions };
+import Svg from './Svg';
+
+type Props = ComponentProps<'svg'> & {
+  dimensions: RadialDimensions;
+  style?: stylex.StyleXStyles;
+};
 
 export default forwardRef<SVGSVGElement, Props>(function Chart(
-  { className = '', children, dimensions, ...rest },
+  { className, children, style, dimensions, ...rest },
   ref
 ) {
   return (
     <Svg
       className={` ${className}`}
       {...rest}
+      // TODO: fix
+      {...stylex.props(style as any)}
       width={dimensions.size}
       height={dimensions.size}
       ref={ref}
