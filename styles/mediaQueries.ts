4a05c5a12a002ad74e8c7ffcda2d132e85eaa363
Dušan Miletić <d@miletich.cc>
2023-12-21 16:23:22 +0100

    define media queries

---
+++ styles/mediaQueries.ts
@@ -0,0 +1,9 @@
+import * as stylex from '@stylexjs/stylex';
+
+export const SM = '@media (min-width: 640px)';
+export const MD = '@media (min-width: 768px)';
+export const LG = '@media (min-width: 1024px)';
+export const XL = '@media (min-width: 1280px)';
+export const XXL = '@media (min-width: 1536px)';
+
+export const DARK = '@media (prefers-color-scheme: dark)';
