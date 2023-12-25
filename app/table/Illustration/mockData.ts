395f0341e33460169bed61dccfaa07f15e76f295
Dušan Miletić <d@miletich.cc>
2023-12-25 10:45:14 +0100

    prettify table illustration

--- app/table/Illustration/mockData.ts
+++ app/table/Illustration/mockData.ts
@@ -2,22 +2,6 @@ import type { Datum } from '@/utils/data';
 
 export const mockData: Pick<Datum, 'datetime' | 'uvindex'>[] = [
   {
-    datetime: '2022-01-01',
-    uvindex: 3,
-  },
-  {
-    datetime: '2022-02-03',
-    uvindex: 4,
-  },
-  {
-    datetime: '2022-03-05',
-    uvindex: 4,
-  },
-  {
-    datetime: '2022-04-04',
-    uvindex: 3,
-  },
-  {
     datetime: '2022-05-05',
     uvindex: 4,
   },
@@ -51,6 +35,22 @@ export const mockData: Pick<Datum, 'datetime' | 'uvindex'>[] = [
   },
   {
     datetime: '2023-01-02',
+    uvindex: 3,
+  },
+  {
+    datetime: '2022-01-01',
+    uvindex: 3,
+  },
+  {
+    datetime: '2022-02-03',
+    uvindex: 4,
+  },
+  {
+    datetime: '2022-03-05',
+    uvindex: 2,
+  },
+  {
+    datetime: '2022-04-04',
     uvindex: 3,
   },
 ];
