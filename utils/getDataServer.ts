9fb7403992810122929d6b655e88917fe1b97dc6
Dušan Miletić <d@miletich.cc>
2023-12-28 16:36:09 +0100

    use selectedYear in getDataServer

--- utils/getDataServer.ts
+++ utils/getDataServer.ts
@@ -2,10 +2,12 @@ import { readFile } from 'fs/promises';
 import * as d3 from 'd3';
 
 import { type Datum, dataSchema } from './data';
+import { selectedYear } from './selectedYear';
 
 type GetDataServer = () => Promise<Datum[]>;
 const getDataServer: GetDataServer = async () => {
-  const csv = await d3.csv(process.env.URL + '/sd-weather-2022.csv');
+  const year = selectedYear.getYear();
+  const csv = await d3.csv(process.env.URL + `/sd-weather-${year}.csv`);
 
   return dataSchema.parse(csv);
 };
